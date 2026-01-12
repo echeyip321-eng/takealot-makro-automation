3023fe5d3289976a0793d75544dca05b6afe9c6d
echeyip321-eng <echeyip321@gmail.com>
2026-01-12 09:33:51 +0200

    Fix line 227 - add missing closing parenthesis

--- main.py
+++ main.py
@@ -224,5 +224,5 @@ def activate_mode(makro_api, review_queue, takealot_scraper, fsn_finder):
         approved_items = review_queue.get_approved_items()        
 
         if not approved_items:
-        logger.info("No approved items to process"
+        logger.info("No approved items to process")
         return
